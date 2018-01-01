4bb2b96d00ec877c38d1d1f031f489fbaa063236
ubansi <goakafu@gmail.com>
2018-01-01 21:57:38 +0900

    add path2file test

--- test/test_file_util.js
+++ test/test_file_util.js
@@ -4,7 +4,7 @@ const fileUtil = require('../lib/modules/fileUtil');
 
 describe('file util test', () => {
 
-  describe('isDirectoryTest', () => {
+  describe('isDirectory test', () => {
     it('directory true test', () => {
       assert(fileUtil.isDirectory('./test'), 'test dir');
     });
@@ -18,4 +18,24 @@ describe('file util test', () => {
     });
   });
 
+  describe('path2File test', () => {
+    const testPath1 = 'aaa/bbb/ccc';
+    const testPath2 = 'aaa/bbb/ccc/';
+    
+    it(testPath1 + ' test', () => {
+      assert.equal(fileUtil.path2File(testPath1), 'ccc', 'normal path');
+    });
+
+    it(testPath2 + ' test', () => {
+      assert.equal(fileUtil.path2File(testPath2 + '/'), null, 'normal path');
+    });
+
+    it('null path test', () => {
+      assert.equal(fileUtil.path2File(null), null, 'normal path');
+    });
+
+    it('void path test', () => {
+      assert.equal(fileUtil.path2File(''), null, 'normal path');
+    });
+  });
 });
